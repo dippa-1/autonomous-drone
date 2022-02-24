e2669d14ecac9e67eff636cf1f03d2e36f1631aa
dippa-1 <dominik.helfenstein@gmx.de>
2022-02-24 22:52:22 +0100

    added http server

---
+++ io-container/http_server.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HTTPHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("hi!".encode("utf8"))
+
+if __name__ == "__main__":
+    server_address = ("localhost", 8000)
+    httpd = HTTPServer(server_address, HTTPHandler)
+
+    print(f"Starting httpd server on {server_address[0]}:{server_address[1]}")
+    httpd.serve_forever()
